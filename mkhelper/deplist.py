57fdcdd2c2dc9f8681dd76576df4ce32f517a3c7
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2018-09-04 10:17:04 +0200

    deplist.py: do not open all input files at once.

--- mkhelper/deplist.py
+++ mkhelper/deplist.py
@@ -18,7 +18,6 @@ def parse_args():
                              'prerequisites (default: %(default)s)')
     parser.add_argument('makefile',
                         metavar='MAKEFILE', nargs='+',
-                        type=argparse.FileType('r'),
                         help='path to a simple makefile that contains only '
                              'dependencies (no recipes)')
     return parser.parse_args()
@@ -35,22 +34,23 @@ def pick_prerequisites(dep_graph, target, result=set()):
 
 
 def parse_dep_file_to_dict(dep_file, result):
-    line = dep_file.readline()
-    while line:
-        while line.endswith('\\\n'):
-            line = line[:-2] + dep_file.readline()
-        split = line.strip().split(':', 1)
-        if split[0]:
-            targets = set(split[0].split())
-            new_prerequisites = set(
-                split[1].replace('|', ' ').replace(':', ' ').split())
-            for t in targets:
-                old_prerequisites = result.get(t, None)
-                if old_prerequisites:
-                    old_prerequisites.update(new_prerequisites)
-                else:
-                    result[t] = new_prerequisites
-        line = dep_file.readline()
+    with open(dep_file, 'r') as f:
+        line = f.readline()
+        while line:
+            while line.endswith('\\\n'):
+                line = line[:-2] + f.readline()
+            split = line.strip().split(':', 1)
+            if split[0]:
+                targets = set(split[0].split())
+                new_prerequisites = set(
+                    split[1].replace('|', ' ').replace(':', ' ').split())
+                for t in targets:
+                    old_prerequisites = result.get(t, None)
+                    if old_prerequisites:
+                        old_prerequisites.update(new_prerequisites)
+                    else:
+                        result[t] = new_prerequisites
+            line = f.readline()
 
 
 def main():
