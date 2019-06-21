e7b858e79b1d8d0cb5a9ca1b11570b22dc54ccdd
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2019-06-21 18:22:36 +0200

    libtool: add a patch removing duplicates of compilation and linking flags.

--- autogen.sh
+++ autogen.sh
@@ -4,13 +4,13 @@ autoreconf -fvi || exit $?
 
 if test -f m4/libtool.m4; then
   # An example of how libtool patches can be applied:
-  patch --forward --no-backup-if-mismatch -p1 -r - -i libtool_patches/libtool.m4.nag_wrapper.patch -d .
+  patch --forward --no-backup-if-mismatch -p1 -r - -i libtool_patches/libtool.m4.nag_wrapper.patch
 
   # The program 'patch' exits with exitcode=1 if the patch has already been applied.
   # Consider this a normal scenario:
   exitcode=$?; test $exitcode -ne 0 && test $exitcode -ne 1 && exit $exitcode
 
-  patch --forward --no-backup-if-mismatch -p1 -r - -i libtool_patches/libtool.m4.arg_spaces.patch -d .
+  patch --forward --no-backup-if-mismatch -p1 -r - -i libtool_patches/libtool.m4.arg_spaces.patch
   exitcode=$?; test $exitcode -ne 0 && test $exitcode -ne 1 && exit $exitcode
 
   # Rebuild configure if you need to patch M4 macros:
@@ -20,7 +20,13 @@ if test -f m4/libtool.m4; then
   # checks them (e.g. makefiles are generated with Automake):
   touch -r m4/ltversion.m4 m4/libtool.m4
 
-  # The following patch modifies only ltmain.sh, so it does not require re-running autoconf:
-  patch --forward --no-backup-if-mismatch -p1 -r - -i libtool_patches/ltmain.sh.nag_pthread.patch -d .
+  # The following patches modify only ltmain.sh, so they do not require re-running autoconf:
+  patch --forward --no-backup-if-mismatch -p1 -r - -i libtool_patches/ltmain.sh.nag_pthread.patch
   exitcode=$?; test $exitcode -ne 0 && test $exitcode -ne 1 && exit $exitcode
+
+  patch --forward --no-backup-if-mismatch -p1 -r - -i libtool_patches/ltmain.sh.no_flag_duplication.patch
+  exitcode=$?; test $exitcode -ne 0 && test $exitcode -ne 1 && exit $exitcode
+
+  # All went fine since we have not exited before:
+  exit 0
 fi
