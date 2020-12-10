ff88b5da90b4e4d2f401d6791e7d5189d362cb40
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2020-12-10 18:58:11 +0100

    fortmodcmp.py: add support for OMNI.

--- mkhelper/fortmodcmp.py
+++ mkhelper/fortmodcmp.py
@@ -67,6 +67,82 @@ def mods_differ(filename1, filename2, compiler_name=None):
                 if not (skip_sequence(stream1, magic_sequence) and
                         skip_sequence(stream2, magic_sequence)):
                     return True
+            elif compiler_name == "omni":
+                import xml.etree.ElementTree as eT
+                # Attributes that either declare or reference the type hashes.
+                # Each list contains a group of tags that reference "same
+                # things".
+                hash_attrs = [["imported_id"],
+                              ["type", "ref", "return_type", "extends"]]
+
+                tree1 = eT.parse(stream1)
+                tree2 = eT.parse(stream2)
+
+                try:
+                    it1 = tree1.iter()
+                    it2 = tree2.iter()
+                except AttributeError:
+                    it1 = iter(tree1.getiterator())
+                    it2 = iter(tree2.getiterator())
+
+                type_maps1 = [dict() for _ in hash_attrs]
+                type_maps2 = [dict() for _ in hash_attrs]
+
+                for node1 in it1:
+                    try:
+                        node2 = next(it2)
+                    except StopIteration:
+                        # The second file is shorter:
+                        return True
+
+                    if node1.tag != node2.tag:
+                        # The nodes have different tags:
+                        return True
+
+                    for ii, attr_group in enumerate(hash_attrs):
+                        type_map1 = type_maps1[ii]
+                        type_map2 = type_maps2[ii]
+
+                        for attr in attr_group:
+                            if (attr in node1.attrib) != \
+                                    (attr in node2.attrib):
+                                # One of the files has the attribute and the
+                                # second one doesn't:
+                                return True
+
+                            hash1 = node1.attrib.pop(attr, None)
+                            hash2 = node2.attrib.pop(attr, None)
+
+                            if hash1 == hash2:
+                                # Either the attribute is missing in both nodes
+                                # or they have the same value:
+                                continue
+                            elif (hash1 in type_map1) != \
+                                    (hash2 in type_map2):
+                                # One of the files has already declared the
+                                # respective hash and the second one hasn't:
+                                return True
+                            elif hash1 in type_map1 and \
+                                    type_map1[hash1] != type_map2[hash2]:
+                                # Both files have declared the respective hashes
+                                # but they refer to different types:
+                                return True
+                            else:
+                                # Declare the respective hashes for both files:
+                                type_value = len(type_map1)
+                                type_map1[hash1] = type_value
+                                type_map2[hash2] = type_value
+
+                    if node1.attrib != node2.attrib:
+                        # The rest of the attributes have different values:
+                        return True
+                try:
+                    next(it2)
+                    # The first file is shorter:
+                    return True
+                except StopIteration:
+                    return False
+
             # Compare the rest (or everything for unknown compilers):
             while 1:
                 buf1 = stream1.read(BUF_MAX_SIZE)
