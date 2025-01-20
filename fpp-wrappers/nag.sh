36842c52c572b95b0c75ed7448388167a022bd50
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2025-01-20 12:07:02 +0100

    misc: fix file ends and trailing whitespaces.

--- fpp-wrappers/nag.sh
+++ fpp-wrappers/nag.sh
@@ -36,10 +36,10 @@
 #   4) '-Wp,-w' prevents 'fpp', which is internally used by the
 #      compiler, from emitting warning messages, including the
 #      one that says "fpp: warning: bad option: - , ignored").
-# 
+#
 # However, the compiler might not be able to compile the output of its own
 # preprocessor. For example:
-# 
+#
 #   $ cat ./conftest.f90
 #   program main
 #     print *, ("A very very very very very very very very very very very very long line of code. We need more than 132 characters on this line. &
@@ -55,17 +55,17 @@
 #   [NAG Fortran Compiler pass 1 error termination, 3 errors]
 #
 # There are three known ways to solve this problem:
-# 
+#
 #   1) compile the output of the preprocessing without the flag '-fpp', which
 #      is apparently the way intended by the developers:
-# 
+#
 #      $ nagfor ./conftest_preprocessed.f90
 #      NAG Fortran Compiler Release 6.2(Chiyoda) Build 6223
 #      [NAG Fortran Compiler normal termination]
-# 
+#
 #      however, we might need to introduce additional preprocessor directives
 #      to the preprocessed file before compiling it;
-# 
+#
 #   2) disable the generation of line numbering directives by extending the
 #      preprocessor command with the '-Wp,-P' flag:
 #
@@ -74,13 +74,13 @@
 #      $ nagfor -fpp ./conftest_preprocessed.f90
 #      NAG Fortran Compiler Release 6.2(Chiyoda) Build 6223
 #      [NAG Fortran Compiler normal termination]
-# 
+#
 #      however, we might want to keep the directives, e.g. in order to know the
 #      files that were included by the preprocessor (there is flag '-Wp,-M'
 #      that is meant for that but other compilers might have different flags
 #      and/or different logic for this feature, which would require additional
 #      customization of the dependency generation);
-# 
+#
 #   3) clean the preprocessor output from the problematic directives, which
 #      is implemented in this script.
 
@@ -94,12 +94,12 @@ pp_args='-fpp -F -o - -Wp,-w'
 # banana&
 # # 123
 # &orange
-# 
+#
 # with:
-# 
+#
 # banana&
 # &orange
-# 
+#
 # The script should work with both GNU and BSD versions of sed:
 sed_script='N;$q;s/&\n# [1-9][0-9]*/\&/;P;D'
 
